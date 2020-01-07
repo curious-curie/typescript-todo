a50bfd32806dcb11d85519d5b8a2db415f3b461c
curious-curie <curious.curieyoo@gmail.com>
2020-01-07 11:59:49 +0900

    Initial dist subtree commit

---
+++ precache-manifest.6c97e84bd3e3218cd0ca5944e3022327.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdca1538f3245bbbf001",
+    "url": "typescript-todo/css/app.8ad8dea7.css"
+  },
+  {
+    "revision": "0d94828f25b7367fd05f0b67d27c49ce",
+    "url": "typescript-todo/index.html"
+  },
+  {
+    "revision": "fdca1538f3245bbbf001",
+    "url": "typescript-todo/js/app.c820e159.js"
+  },
+  {
+    "revision": "d8920f3003bbfccbb7a3",
+    "url": "typescript-todo/js/chunk-vendors.d63e68bf.js"
+  },
+  {
+    "revision": "fc9ada481bd35ed059b7b91ebb5baa9a",
+    "url": "typescript-todo/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "typescript-todo/robots.txt"
+  }
+]);
