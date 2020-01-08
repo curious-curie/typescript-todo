47e5f0893a1b5773eb9aaa06a490ab08e6b2bb86
curious-curie <curious.curieyoo@gmail.com>
2020-01-08 22:27:47 +0900

    Fix: modify actions to connect todos with each users - created subcollection todos per each user in collection users - so that users can only access to their own todos

--- src/store/modules/auth/actions.ts
+++ src/store/modules/auth/actions.ts
@@ -1,29 +1,37 @@
-import db from '@/main'
 import { State } from './types'
 import { Commit } from 'vuex'
-import * as firebase from 'firebase'
+import * as firebase from 'firebase/app'
+import db from '@/main'
 
 export default {
   register: (context: { commit: Commit, state: State }, payload: any) => {
     firebase.auth().createUserWithEmailAndPassword(payload.email, payload.password)
       .then(
         res => {
-          console.log(res)
           context.commit('SET_USER', res.user)
+          if (res.user !== null) {
+            const newUser = {
+              'uid': res.user.uid,
+              'email': res.user.email
+            }
+            db.collection('users').doc(res.user.uid)
+              .set(newUser)
+          }
         }
       )
+      .catch((error) => {
+        alert(error.message)
+      })
   },
   login (context: { commit: Commit, state: State }, payload: any) {
     firebase.auth().signInWithEmailAndPassword(payload.email, payload.password)
       .then(
         res => {
-          console.log(res)
           context.commit('SET_USER', res.user)
         }
       )
       .catch((error) => {
         alert(error.message)
-        console.log(error)
       })
   },
   logout (context: { commit: Commit, state: State }, payload: any) {
--- src/store/modules/todos/actions.ts
+++ src/store/modules/todos/actions.ts
@@ -1,22 +1,25 @@
 import db from '@/main'
 import { State, TodoConfig } from './types'
 import { Commit } from 'vuex'
+import * as firebase from 'firebase/app'
 
 export default {
   loadTodos: (context: { commit: Commit, state: State }, payload: object) => {
     context.commit('LOAD_TODOS', payload)
   },
   addTodo (context: { commit: Commit, state: State }) {
-    if (context.state.newTodo) {
+    if (context.state.newTodo && firebase.auth().currentUser !== null) {
+      const userId = firebase.auth().currentUser!.uid
       const todoItem = {
         title: context.state.newTodo,
         completed: false
       }
       context.commit('ADD_TODO', todoItem)
-      db.collection('todos').doc(todoItem.title).set({
-        title: context.state.newTodo,
-        completed: false
-      })
+      // db.collection('todos').doc(todoItem.title).set({
+      //   title: context.state.newTodo,
+      //   completed: false
+      // })
+      db.collection('users').doc(userId).collection('todos').doc(todoItem.title).set(todoItem)
     }
   },
   setNewTodo (context: { commit: Commit, state: State }, todoInput: string) {
@@ -26,26 +29,31 @@ export default {
     context.commit('CLEAR_NEW_TODO')
   },
   deleteTodo (context: { commit: Commit, state: State }, todo: TodoConfig) {
+    const userId = firebase.auth().currentUser!.uid
     context.commit('DELETE_TODO', todo)
     db.collection('todos')
       .doc(todo.title)
       .delete()
+    db.collection('users').doc(userId).collection('todos').doc(todo.title).delete()
   },
   toggleTodo (context: { commit: Commit, state: State }, todo: TodoConfig) {
+    const userId = firebase.auth().currentUser!.uid
     context.commit('TOGGLE_TODO', todo)
     const todoItem = { ...todo }
     todoItem.completed = !todo.completed
-    db.collection('todos')
+    db.collection('users').doc(userId)
+      .collection('todos')
       .doc(todo.title)
       .set(todoItem)
   },
   clearCompleted (context: { commit: Commit, state: State }) {
+    const userId = firebase.auth().currentUser!.uid
     context.commit('CLEAR_COMPLETED')
-    db.collection('todos').get()
+    db.collection('users').doc(userId).collection('todos').get()
       .then(snapshot => {
         snapshot.forEach(doc => {
           if (doc.data().completed) {
-            db.collection('todos')
+            db.collection('users').doc(userId).collection('todos')
               .doc(doc.data().title)
               .delete()
           }
@@ -53,12 +61,13 @@ export default {
       })
   },
   completeAll (context: { commit: Commit, state: State }) {
+    const userId = firebase.auth().currentUser!.uid
     context.commit('COMPLETE_ALL')
-    db.collection('todos').get()
+    db.collection('users').doc(userId).collection('todos').get()
       .then(snapshot => {
         snapshot.forEach(doc => {
           if (!doc.data().completed) {
-            db.collection('todos')
+            db.collection('users').doc(userId).collection('todos')
               .doc(doc.data().title)
               .set(
                 { ...doc.data(), completed: true }
